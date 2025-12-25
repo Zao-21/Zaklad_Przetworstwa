5c9ce612e3f5d18eb887e2a56a21ecc092ff39ca
Zao-21 <zakpatryk675@gmail.com>
2025-12-25 23:39:26 +0100

    Implementacja klasy ProduktPrzetworzony z właściwościami

--- src/zakladprzetworstwa/ProduktPrzetworzony.java
+++ src/zakladprzetworstwa/ProduktPrzetworzony.java
@@ -1,4 +1,26 @@
 package zakladprzetworstwa;
+import java.util.List;
 
 public class ProduktPrzetworzony {
+  private String nazwa;
+    private List<Produkt> skladniki;
+    private double cena;
+    private int ilosc;
+    private TypPrzetworzonegoProduktu typ;
+
+    public ProduktPrzetworzony(String nazwa, List<Produkt> skladniki, double cena, int ilosc, TypPrzetworzonegoProduktu typ) {
+        this.nazwa = nazwa;
+        this.skladniki = skladniki;
+        this.cena = cena;
+        this.ilosc = ilosc;
+        this.typ = typ;
+    }
+
+    public void dodajIlosc(int ilosc) {
+        this.ilosc += ilosc;
+    }
+    public String toString(){
+        return nazwa + " (" + typ + "), ilość: " + ilosc;
+    }
 }
+
