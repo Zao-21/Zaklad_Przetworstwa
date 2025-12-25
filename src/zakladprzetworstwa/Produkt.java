f9c4a4aa6998d0ee235112e7680ddcd63959c948
Zao-21 <zakpatryk675@gmail.com>
2025-12-25 20:30:07 +0100

    Implementacja klasy Produkt z właściwościami i metodami
    Dodano pola i metody umożliwiające zarządzanie szczegółami produktu.

--- src/zakladprzetworstwa/Produkt.java
+++ src/zakladprzetworstwa/Produkt.java
@@ -1,4 +1,36 @@
 package zakladprzetworstwa;
 
 public class Produkt {
+  private String nazwa;
+    private double cena;
+    private int ilosc;
+    private TypProduktu typ;
+
+    public Produkt(String nazwa, double cena, int ilosc, TypProduktu typ) {
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.ilosc = ilosc;
+        this.typ = typ;
+    }
+
+    public void dodajIlosc(int ilosc) {
+        this.ilosc += ilosc;
+    }
+
+    public void odejmijIlosc(int ilosc) {
+        this.ilosc -= ilosc;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public int getIlosc() {
+        return ilosc;
+    }
+
+    public String toString() {
+        return nazwa + "(" + typ + "), ilosc:"+ilosc;
+    }
 }
+
