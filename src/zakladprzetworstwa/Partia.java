379dc313400059accb481da7e4450078cb42380c
Zao-21 <zakpatryk675@gmail.com>
2025-12-28 19:52:04 +0100

    Implement Partia class with attributes and constructor
    Dodano pola i konstruktor do klasy Partia.

--- src/zakladprzetworstwa/Partia.java
+++ src/zakladprzetworstwa/Partia.java
@@ -1,4 +1,23 @@
 package zakladprzetworstwa;
 
+import java.time.LocalDate;
+
 public class Partia {
+    private Produkt produkt;
+    private int iloscSurowca;
+    private LocalDate dataProdukcji;
+
+    public Partia(Produkt produkt, int iloscSurowca, LocalDate dataProdukcji) {
+        this.produkt = produkt;
+        this.iloscSurowca = iloscSurowca;
+        this.dataProdukcji = dataProdukcji;
+    }
+
+    @Override
+    public String toString() {
+        return "Partia: " + produkt.getNazwa() +
+                ", surowiec: " + iloscSurowca +
+                ", data: " + dataProdukcji;
+    }
 }
+
