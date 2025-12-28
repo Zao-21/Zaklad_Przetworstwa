26a21ec532470bcd6cdfbb552767383abe2fd1ce
Zao-21 <zakpatryk675@gmail.com>
2025-12-28 19:51:22 +0100

    Implement Dostawa class with product and delivery info
    Dodano pola i konstruktor w celu zarządzania szczegółami dostawy.

--- src/zakladprzetworstwa/Dostawa.java
+++ src/zakladprzetworstwa/Dostawa.java
@@ -1,4 +1,24 @@
 package zakladprzetworstwa;
 
+import java.time.LocalDate;
+
 public class Dostawa {
+    private Produkt produkt;
+    private int ilosc;
+    private LocalDate dataDostawy;
+
+    public Dostawa(Produkt produkt, int ilosc, LocalDate dataDostawy) {
+        this.produkt = produkt;
+        this.ilosc = ilosc;
+        this.dataDostawy = dataDostawy;
+        produkt.dodajIlosc(ilosc);
+    }
+
+    @Override
+    public String toString() {
+        return "Dostawa: " + produkt.getNazwa() +
+                ", ilość: " + ilosc +
+                ", data: " + dataDostawy;
+    }
 }
+
